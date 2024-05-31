bc27e871a016567d49ac07f33c9567f3f150eae0
Sahil Sapariya <sahilsapariya03@gmail.com>
2024-05-31 21:18:37 +0530

    DONE: register and login action completed

--- actions/register.ts
+++ actions/register.ts
@@ -1,18 +1,44 @@
-"use server"
+"use server";
 
-import { RegisterSchema } from '@/schemas';
-import * as z from 'zod'
+import bcrypt from "bcryptjs";
 
-export const registerAction = async (values: z.infer<typeof RegisterSchema>) => {
-    const validatedFields = RegisterSchema.safeParse(values)
+import { RegisterSchema } from "@/schemas";
+import * as z from "zod";
+import { db } from "@/db/db";
+import { getUserByEmail } from "@/data/user";
 
-    if (!validatedFields.success) {
-        return {
-            error: "Invalid email or password. Please try again.",
-        }
-    }
+export const registerAction = async (
+  values: z.infer<typeof RegisterSchema>
+) => {
+  const validatedFields = RegisterSchema.safeParse(values);
 
+  if (!validatedFields.success) {
     return {
-        success: "Email Sent!",
-    }
-}
+      error: "Invalid email or password. Please try again.",
+    };
+  }
+  const { email, password, name } = validatedFields.data;
+  const hashedPassword = await bcrypt.hash(password, 10);
+
+  const existingUser = await getUserByEmail(email)
+
+  if (existingUser) {
+    return {
+      error: "User already exists",
+    };
+  }
+
+  await db.user.create({
+    data: {
+      email,
+      password: hashedPassword,
+      name,
+    },
+  });
+
+  // TODO: Send email to user
+
+  return {
+    success: "Email Sent!",
+  };
+};
